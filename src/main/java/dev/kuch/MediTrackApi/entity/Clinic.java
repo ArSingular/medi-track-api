a924f6b661471e065a7039d813c7bba3ef28abb8
Artur <kucharthur18@gmail.com>
2025-12-12 15:57:09 +0200

    -Clinic entity added; users table created

---
+++ src/main/java/dev/kuch/MediTrackApi/entity/Clinic.java
@@ -0,0 +1,44 @@
+package dev.kuch.MediTrackApi.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+/**
+ * @author Korol Artur
+ * 12.12.2025
+ */
+
+@Entity
+@Table(name = "clinics")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Clinic {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id", nullable = false)
+    private UUID id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "owner_email", nullable = false)
+    private String ownerEmail;
+
+    @Column(name = "is_active")
+    private boolean isActive;
+
+    @Column(name = "paid_until")
+    private LocalDate paidUntil;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+}
