4d343b616607b6883a82649c55c34de98013a830
Artur <kucharthur18@gmail.com>
2025-12-21 03:17:40 +0200

    -Added fullName method for transaction mapper, also mail dependency added

--- src/main/java/dev/kuch/MediTrackApi/util/mapper/TransactionMapper.java
+++ src/main/java/dev/kuch/MediTrackApi/util/mapper/TransactionMapper.java
@@ -2,8 +2,10 @@ package dev.kuch.MediTrackApi.util.mapper;
 
 import dev.kuch.MediTrackApi.dto.response.TransactionResponse;
 import dev.kuch.MediTrackApi.entity.InventoryTransaction;
+import dev.kuch.MediTrackApi.entity.User;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 import org.mapstruct.NullValueMappingStrategy;
 import org.springframework.stereotype.Component;
 
@@ -19,7 +21,21 @@ public interface TransactionMapper {
     @Mapping(target = "productName", source = "product.name")
     @Mapping(target = "batchNumber", source = "batch.batchNumber")
     @Mapping(target = "timestamp", source = "createdAt")
-    @Mapping(target = "userName", source = "user.email")
+    @Mapping(target = "userName", source = "user", qualifiedByName = "getFullName")
     TransactionResponse toResponse(InventoryTransaction inventoryTransaction);
+
+    @Named("getFullName")
+    default String getFullName(User user){
+        if(user == null) return "UNKNOWN";
+
+        if(user.getFirstName() == null && user.getLastName() == null){
+            return user.getEmail();
+        }
+
+        String firstName = user.getFirstName() != null ? user.getFirstName() : "";
+        String lastName = user.getLastName() != null ? user.getLastName() : "";
+
+        return (firstName + " " + lastName).trim();
+    }
 
 }
